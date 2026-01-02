97feb811a89e60879030dba86b367a31e6be4c96
Yashaswini <yashuurs957@gmail.com>
2026-01-02 11:10:39 +0530

    print project title update

--- student-management-system/main.py
+++ student-management-system/main.py
@@ -1,1 +1,32 @@
-print("student management system")
+students = []
+
+while True:
+ print("\n===== Student Management System =====")
+ print("1. Add Student")
+ print("2. View Students")
+ print("3. Exit")
+
+ choice = input("Enter your choice: ")
+
+ if choice == "1":
+    name = input("Enter student name: ").strip()
+
+    if name == "":
+        print("Name cannot be empty")
+    else:
+        students.append(name)
+        print("Student added successfully")
+
+ elif choice == "2":
+    if not students:
+        print("No students found")
+    else:
+        print("Students:")
+        for student in students:
+            print("-", student)
+
+ elif choice == "3":
+     print("exiting the program")
+
+ else :
+     print("invalid choice")
